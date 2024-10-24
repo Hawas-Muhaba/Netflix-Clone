569a39e3583aaf9647ae56335ec6792628a26f33
Hawas Muhaba <hawasmuhaba4socials@gmail.com>
2024-10-24 14:57:53 +0300

    100% completed of cloning Netflix

--- netflixclone/src/components/Rows/Rowlist/Rowlist.js
+++ netflixclone/src/components/Rows/Rowlist/Rowlist.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import Row from '../Row/Row/Row'
+import React from "react";
+import Row from "../Row/Row/Row";
+import requests from "../../../utils/requests";
 
-const Rowlist = () => {
+const RowList = () => {
   return (
     <>
-      <Row />
-      <Row />
-      <Row />
-      <Row />
-      <Row />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Tv Shows" fetchUrl={requests.fetchTvShow} isLargeRow={true} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
     </>
   );
-}
+};
 
-export default Rowlist
+export default RowList;
